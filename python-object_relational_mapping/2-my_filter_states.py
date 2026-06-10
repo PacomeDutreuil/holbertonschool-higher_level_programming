82d717f49f3e2b5dfcc6f65e24b010a0bb44f82a
PacomeDutreuil <13007@holbertonstudents.com>
2026-06-10 15:27:17 +0200

    Add Task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Displays all values in states where name matches the argument."""
+"""Displays all values in the states table matching the argument."""
 
 import MySQLdb
 import sys
@@ -11,21 +11,19 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        db=sys.argv[3]
     )
 
     cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' "
+        "ORDER BY states.id ASC".format(sys.argv[4])
     )
-    cur.execute(query)
 
-    rows = cur.fetchall()
-
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
     db.close()
+
